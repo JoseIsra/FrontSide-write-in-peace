27d6e472e0d3bb7ca4441b1e2aa4548fb2d1bef8
JoseIsra <israelmyname@gmail.com>
2022-07-20 00:09:00 -0500

    ✨ (landing): Refactor landing styles
    refactor landing styles and setup validations

--- src/components/organism/SignUp/signUp.vue
+++ src/components/organism/SignUp/signUp.vue
@@ -4,10 +4,31 @@
       <h5 class="no-margin no-padding brand text-weight-bolder">MyPeace</h5>
     </template>
     <template #content>
-      <basic-input v-model="userName" label="Nombre" />
-      <basic-input v-model="lastName" label="Apellidos" />
-      <basic-input v-model="email" label="Correo" />
-      <basic-input v-model="password" type="password" label="Contraseña" />
+      <basic-input v-model="v$.userName.$model" label="Nombre" />
+      <basic-input v-model="v$.lastName.$model" label="Apellidos" />
+      <basic-input v-model="v$.email.$model" label="Correo" />
+      <p
+        v-for="error of v$.email.$errors"
+        :key="error.$uid"
+        class="no-margin text-red"
+      >
+        {{ error.$message }}
+      </p>
+      <div class="relative-position">
+        <basic-input
+          v-model="v$.password.$model"
+          type="password"
+          label="Contraseña"
+        />
+        <p
+          v-for="error of v$.password.$errors"
+          :key="error.$uid"
+          class="no-margin text-red absolute"
+          style="top: 100%"
+        >
+          {{ error.$message }}
+        </p>
+      </div>
     </template>
     <template #action>
       <q-btn
@@ -22,23 +43,54 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
+import { defineComponent, reactive, computed } from 'vue';
 import BaseForm from '@/components/organism/BaseForm';
 import BasicInput from '@/components/atoms/BasicInput';
+import useVuelidate from '@vuelidate/core';
+import { required, email, minLength, helpers } from '@vuelidate/validators';
 
 export default defineComponent({
   name: 'Login',
   components: { BaseForm, BasicInput },
   setup() {
-    const email = ref<string>('');
-    const password = ref<string>('');
-    const userName = ref<string>('');
-    const lastName = ref<string>('');
+    const form = reactive({
+      email: '',
+      password: '',
+      userName: '',
+      lastName: '',
+    });
+
+    const rules = computed(() => ({
+      email: {
+        required: helpers.withMessage('No ha escrito un correo', required),
+        email: helpers.withMessage('No es un correo válido', email),
+      },
+      password: {
+        required: helpers.withMessage('No has escrito tu contraseña', required),
+        minLength: helpers.withMessage(
+          'La contraseña necesita mínimo 8 carácteres',
+          minLength(8)
+        ),
+      },
+      userName: {
+        required: helpers.withMessage('No has escrito tu nombre', required),
+      },
+      lastName: {
+        required: helpers.withMessage('No has escrito tus apellidos', required),
+      },
+    }));
+
+    const v$ = useVuelidate(rules, form);
+
+    const submitLogin = async () => {
+      const res = await v$.value.$validate();
+      alert(res);
+    };
+
     return {
-      email,
-      password,
-      userName,
-      lastName,
+      form,
+      v$,
+      submitLogin,
     };
   },
 });
